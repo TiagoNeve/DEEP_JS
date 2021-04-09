7b420de9b020e2bd52681ac95b8d039f34cfdd05
Tiago Neves <rykarumstuler@gmail.com>
2021-04-08 22:36:10 -0300

    Introducao ao modulo de JS da TRYBE

--- SOLOLEARN/dom.js
+++ SOLOLEARN/dom.js
@@ -35,4 +35,18 @@ parent.replaceChild(p, child);
 // Animações 
 // Utilizado o conceito de animações no codepen.io
 
-//
+// Eventos
+/**
+ * onclick -> Ocorre quando um elemento é clicado
+ * onload -> Ocorre quando um objeto é carregado
+ * onunload -> Ocorre quando uma página é descarregada da página body
+ * onchange -> Ocorre quando o conteúdo de um elemento recebe alguma alteração
+ * onmouseouver -> Ocorre quando o mouse passa por cima.
+ * onmouseout -> Ocorre quando o mouse sai do range de um elemento, antes passado
+ * onmousedown -> Ocorre quando o elemento recebe algum click, no momento em que ocorre o
+ * click.
+ * onmouseup -> Ocorre quando o elemento é desclicado pelo mouse que havia clicado antes.
+ * onblur -> Ocorre quando um elemento perde o focus.
+ * onfocus -> Ocorre quando um elemento ganha focus.
+ */
+// A função que é utilizada para referenciar eventos é geralmente chamada de events handle
---
+++ TRYBE/introducao.js
@@ -0,0 +1,18 @@
+/**
+ *  Introdução ao JavaScript -- TRYBE
+ *
+ * Aprender a base do Javascript no curso e depois explorar os frameworks
+ *
+ *  Frameworks -> Jquery / Angular / NodeJs
+ *
+ * O junior não tem tanta experiência no desenvolvimento e sobre os softSkils
+ *
+ * O Pleno, consegue ter um domínio melhor das ferramentas e consegue entender
+ * as regras de negócio da aplicação, além de ter uma experiência boa na
+ * resolução de problemas e maior engajamento na resolução dos problemas
+ *
+ * O sênior é capaz de se autogerir, consegue ter uma autonomia muito boa
+ * e está constantemente ativo na resolução de problemas e regras de negócio
+ * do problema. Além de ser uma referência para a equipe de desenvolvedores
+ * e tem um amplo domínio das tecnologias que estão sendo executadas.
+ */
