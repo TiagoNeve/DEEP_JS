1b5e1d988dd257699d9e6f09feb2e7d80be05132
Tiago Neves <rykarumstuler@gmail.com>
2021-04-11 10:11:31 -0300

    freeCodeCamp methods

--- freeCodeCamp/basic.js
+++ freeCodeCamp/basic.js
@@ -6,5 +6,6 @@
  * .push() -> Adiciona um valor no último index de um array
  * .put() -> Remove o último index de um array
  * .shift() -> Remove o primeiro index de um array
+ * .unshift() -> Adiciona um valor no primeiro index do array
  *
  */
