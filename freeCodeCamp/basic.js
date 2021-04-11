ba08fa5970c2d1d6c79a7729f9f5244953914e07
Tiago Neves <rykarumstuler@gmail.com>
2021-04-11 02:10:22 -0300

    freeCodeCamp methods

---
+++ freeCodeCamp/basic.js
@@ -0,0 +1,10 @@
+/**
+ * Comentários no javascript é isso.
+ * Pode-se comentar utilizando // ou /** */
+
+/**
+ * .push() -> Adiciona um valor no último index de um array
+ * .put() -> Remove o último index de um array
+ * .shift() -> Remove o primeiro index de um array
+ *
+ */
