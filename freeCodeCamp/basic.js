45527b842f6e7a287109591ae96ba991688e5f58
Tiago Neves <rykarumstuler@gmail.com>
2021-04-11 22:20:05 -0300

    freeCodeCamp dica de objetos ao inves de switch case

--- freeCodeCamp/basic.js
+++ freeCodeCamp/basic.js
@@ -9,3 +9,21 @@
  * .unshift() -> Adiciona um valor no primeiro index do array
  *
  */
+
+var myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"],
+    "bark": "woof"
+};
+
+// Para deletar uma propriedade de um objeto:
+delete myDog.tails;
+
+/* Se estiver utilizando um switch case para fazer verificação de strings com apenas um return
+    O melhor seria criar um objeto com os valores de retorno e a comparação, depois chamar a
+    verificação utilizando o conceito de blocos para verificar se existe uma chave no objeto
+    e atribuir o retorno da chave em uma variável. Pronto, fez a verificação sem precisar utilizar
+    o switch case.
+*/
