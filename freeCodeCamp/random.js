6a4780d5e7c7ab582ea42035f25dc478c9d3f4cd
Tiago Neves <rykarumstuler@gmail.com>
2021-04-16 22:57:53 -0300

    Recursividade Sololearn

--- freeCodeCamp/random.js
+++ freeCodeCamp/random.js
@@ -27,3 +27,35 @@ function checkSign(num) {
     return (num > 0) ? "positive" :
         (num < 0) ? "negative" : "zero";
 }
+
+function countdown(n) {
+    if (n < 1) {
+        return [];
+    } else {
+        const countArray = countdown(n - 1);
+        countArray.unshift(n);
+        return countArray;
+    }
+}
+
+console.log(countdown(5));
+
+function countup(n) {
+    if (n < 1) {
+        return [];
+    } else {
+        const countArray = countup(n - 1);
+        countArray.push(n);
+        return countArray;
+    }
+}
+console.log(countup(5));
+
+function fatorial(n) {
+    if (n === 1) {
+        return 1;
+    }
+    return n * fatorial(n - 1);
+}
+
+console.log(fatorial(20));
