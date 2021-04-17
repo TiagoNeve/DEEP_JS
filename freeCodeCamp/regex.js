2740f70ea78bb2d0a8e3671887e48b17168fbf09
Tiago Neves <rykarumstuler@gmail.com>
2021-04-17 20:44:50 -0300

    Regex freeCodeCamp

---
+++ freeCodeCamp/regex.js
@@ -0,0 +1,5 @@
+// Para encontrar valores de caracteres em uma string deve colocar /text/
+let testStr = "freeCodeCamp";
+let testRegex = /Code/;
+
+testRegex.test(testRegex) // Retorna true se o regex for válido
