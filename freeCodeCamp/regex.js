663659a07e2a8c858ee690c3256ca2d4666e390d
Tiago Neves <rykarumstuler@gmail.com>
2021-04-17 22:24:56 -0300

    Regex freeCodeCamp

--- freeCodeCamp/regex.js
+++ freeCodeCamp/regex.js
@@ -3,3 +3,9 @@ let testStr = "freeCodeCamp";
 let testRegex = /Code/;
 
 testRegex.test(testRegex) // Retorna true se o regex for válido
+
+// Para verificar ocorrencias no modo insesible case use: i ao final do regex
+testRegex = /Code/i
+
+// Para verificar a quantidade de ocorrencias, use: g ao final do redex
+testRegex = /Code/gi
