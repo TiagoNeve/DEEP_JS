78b7c1b6b7d92b12f844aa32db6559834c78dc4b
Tiago Neves <rykarumstuler@gmail.com>
2021-04-22 23:32:33 -0300

    RocketSeat activities

--- rocketSeat/ex01.js
+++ rocketSeat/ex01.js
@@ -4,11 +4,11 @@ let weigth
 
 // 2 - Que tipo de dado é a variável acima ? 
 
-console.log(typeof weigth) // undefined
+console.log(typeof weight) // undefined
 
 // 3 - Declare uma variável e atribua valores para cada um dos dados;
 
-const name = "Tiago"
+let name = "Tiago"
 const age = 21
 const stars = 3.5
 const isSubscribed = true
@@ -17,3 +17,16 @@ const isSubscribed = true
 let student = {}
 
 console.log(typeof student) // Object
+
+// 4.1 - Atribua a variável student os mesmo atributos e valores da questão 3
+
+student = {
+    name: "Tiago",
+    age: 21,
+    weight: 69.9,
+    isSubscribed: true
+}
+
+// 4.2 - Mostre no console a seguinte mensagem: <name> de idade <age> pesa <weigth>
+
+console.log(`${student.name} de idade ${student.age} pesa ${student.weight}`)
