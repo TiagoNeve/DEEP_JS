b7f6330964dd7b338fb5d2f01979fca5f8c7e0a1
Tiago Neves <rykarumstuler@gmail.com>
2021-04-24 00:00:44 -0300

    RocketSeat exercicies

---
+++ rocketSeat/ex03.js
@@ -0,0 +1,49 @@
+// Sistema de gastos familiares.
+
+const familyAmount = {
+    receitas: [
+        1000,
+        100,
+        300,
+        600,
+        25
+    ],
+    despesas: [
+        300,
+        200,
+        150
+    ]
+}
+
+function getTotalFamilyAmount(control = "") {
+
+    if (control !== "") {
+        throw "Essa função não aceita argumentos!";
+    };
+
+
+    let total = 0;
+    let amountReceitas = 0;
+    let amountDespesas = 0;
+
+
+    for (let receita of familyAmount.receitas) {
+        amountReceitas += receita;
+    };
+
+    for (let despesa of familyAmount.despesas) {
+        amountDespesas += despesa;
+    };
+
+    total = amountReceitas - amountDespesas;
+
+    return total > 0 ? `Saldo positivo: ${total}` : `Saldo negativo ${total}`;
+}
+
+
+try {
+    const result = getTotalFamilyAmount()
+    console.log(result)
+} catch (e) {
+    console.log(e);
+};
