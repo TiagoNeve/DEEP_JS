2792e9868a7c8d70eb844dca93a1a7a591746996
Tiago Neves <rykarumstuler@gmail.com>
2021-04-23 23:31:25 -0300

    RocketSeat exercicies

--- rocketSeat/ex02.js
+++ rocketSeat/ex02.js
@@ -1,2 +1,35 @@
 // Exercícios do fim do discovery
 
+// 01 - Transformar notas escolares.
+
+
+function transformSchollScore(score = "") {
+    if (score == "") {
+        throw "Informe uma nota para transforma-la"
+    };
+
+    let newScore;
+
+    if (score >= 90) {
+        newScore = "A"
+    } else if (score >= 80) {
+        newScore = "B"
+    } else if (score >= 70) {
+        newScore = "C"
+    } else if (score >= 60) {
+        newScore = "D"
+    } else {
+        newScore = "F"
+    }
+
+    return newScore;
+}
+
+try {
+
+    result = transformSchollScore()
+    console.log(result)
+
+} catch (e) {
+    console.log(e)
+}
