20c5c5e12e857a4d3d4cc17a5fba007f34f11e39
Tiago Neves <rykarumstuler@gmail.com>
2021-04-28 23:07:37 -0300

    Eloquent JS

---
+++ Eloquent/TrianguloComLoop.js
@@ -0,0 +1,11 @@
+/**
+ *  Triângulo com Loop
+ *
+ *  Crie um programa que faça um triângulo retângulo utilizando loops.
+ *  O triângulo precisa ter 7 linhas
+ */
+let row = "";
+for (let i = 0; i < 7; i++) {
+    row += "#";
+    console.log(row);
+};
--- Eloquent/cap01.js
+++ Eloquent/cap01.js
@@ -8,3 +8,31 @@ function fac(n) {
 }
 
 console.log(fac(8))
+
+// ===========================================
+var theNumber = Number(prompt("Digite um número: "))
+if (!isNaN(theNumber)) {
+    alert("Seu número é: ", theNumber)
+}
+
+
+// =============================================
+var result = 1;
+var counter = 0;
+while (counter < 10) {
+    result = result * 2;
+    counter = counter + 1;
+}
+console.log(result);
+
+// ===============================================
+do {
+    var nome = prompt("Quem é você?");
+} while (!nome);
+console.log(nome);
+// ===============================================
+var result = 1;
+for (var counter = 0; contador < 10; counter = counter + 1) {
+    result = result * 2;
+};
+console.log(result)
