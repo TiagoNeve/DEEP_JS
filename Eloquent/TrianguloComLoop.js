070adb51c1e734b6a582cf4e88ec3c4293163191
Tiago Neves <rykarumstuler@gmail.com>
2021-04-28 23:14:50 -0300

    Eloquent JS

--- Eloquent/TrianguloComLoop.js
+++ Eloquent/TrianguloComLoop.js
@@ -9,3 +9,21 @@ for (let i = 0; i < 7; i++) {
     row += "#";
     console.log(row);
 };
+
+// Outras formas de resolver o mesmo problema
+console.log(" ====  Outra forma de resolver o mesmo problema ===")
+let row2 = "";
+while (true) {
+    row2 += "#";
+    console.log(row2);
+    if (row2.length == 7) {
+        break;
+    }
+};
+
+console.log("=== Outra forma de resolver o mesmo problema ===");
+let row3 = "";
+do {
+    row3 += "#";
+    console.log(row3);
+} while (row3.length < 7)
