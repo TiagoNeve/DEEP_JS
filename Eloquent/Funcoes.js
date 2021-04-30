43bbe6b8d9f4cb3d3aeebb0cf608567b52534412
Tiago Neves <rykarumstuler@gmail.com>
2021-04-29 22:18:45 -0300

    Eloquent JS

--- Eloquent/Funcoes.js
+++ Eloquent/Funcoes.js
@@ -76,3 +76,19 @@ function power3(base, expoente) {
 }
 
 console.log(power3(2, 2))
+
+// Recursividade na prática : Será que irei compreender essa lógica com primazia ?? 
+function findSolution(target) {
+    function find(start, history) {
+        if (start == target) {
+            return history;
+        } else if (start > target) {
+            return null;
+        } else {
+            return find(start + 5, `( ${history} + 5 )`) || find(start * 3, `( ${history} * 3 )`)
+        }
+    }
+    return find(1, "1");
+}
+
+console.log(findSolution(24))
